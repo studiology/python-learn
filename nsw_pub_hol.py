5bdaf446c2888097bd5a436e6eb1a5af9c538755
studiology <adamk@studiology.nu>
2015-06-04 18:24:44 +1000

    new nsw_pub_hol.py grabs public holidays from data.gov.au and extracts NSW holidays
    learning python very slowly

---
+++ nsw_pub_hol.py
@@ -0,0 +1,19 @@
+# gets Aus public holidays from data.gov.au
+
+import json
+import urllib
+from datetime import date
+from datetime import datetime
+
+url = 'http://data.gov.au/api/action/datastore_search?resource_id=31eec35e-1de6-4f04-9703-9be1d43d405b'
+
+phjs = json.load(urllib.urlopen(url))
+
+for hol in phjs['result']['records']:
+    if ('NAT' in hol['Applicable To'] or 'NSW' in hol['Applicable To']):
+        d = datetime.strptime(hol["Date"],"%Y%m%d")
+        if d > datetime.now():
+            print d.strftime('%a %Y-%m-%d'), hol['Holiday Name']
+        else:
+            print d.strftime('%a %Y-%m-%d'), hol['Holiday Name'], '*** MISSED IT ***'
+
